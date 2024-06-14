c5d78757f1e7a05f1cab31a2f6d80ed3c0f69259
dunossauro <mendesxeduardo@gmail.com>
2024-06-13 22:10:05 -0300

    Iniciando o projeto

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (organização)
+
+    response = client.get('/')  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert

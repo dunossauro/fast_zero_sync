0d6785040a242278a8059554ddba79c2995a0012
dunossauro <mendesxeduardo@gmail.com>
2024-06-18 21:30:12 -0300

    Aula 02 -

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}

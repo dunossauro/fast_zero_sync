6c6fd2b44e88920a1eaf24b80f205f2aeed9d0fd
dunossauro <mendesxeduardo@gmail.com>
2024-07-23 20:47:13 -0300

    Adicionando quebradeira ao CI para fins educativos :fire:

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ from http import HTTPStatus
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
 
-    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.status_code == HTTPStatus.NOT_FOUND  # assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
